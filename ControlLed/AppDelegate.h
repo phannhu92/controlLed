a3884a3b6eb52d2f55dbc4f638b59beca39b7849
Phan Nhu <nhu@misfit.com>
2015-12-19 17:20:06 +0700

    first commit

---
+++ ControlLed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ControlLed
+//
+//  Created by Phan Nhu on 12/19/15.
+//  Copyright © 2015 MFM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ControlLed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ControlLed
+//
+//  Created by Phan Nhu on 12/19/15.
+//  Copyright © 2015 MFM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
